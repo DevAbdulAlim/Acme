08c3975672bc39bdee9d52339476e92b86c0b923
Abdul Alim <aa.abdulalim13@gmail.com>
2023-12-12 13:18:35 +0600

    added prisma with seeding

---
+++ prisma/migrations/20231212061541_initial/migration.sql
@@ -0,0 +1,51 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Customer" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "image_url" TEXT,
+
+    CONSTRAINT "Customer_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Invoice" (
+    "id" TEXT NOT NULL,
+    "customer_id" TEXT NOT NULL,
+    "amount" DOUBLE PRECISION NOT NULL,
+    "status" TEXT NOT NULL,
+    "date" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Invoice_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Revenue" (
+    "id" TEXT NOT NULL,
+    "month" TEXT NOT NULL,
+    "revenue" DOUBLE PRECISION NOT NULL,
+
+    CONSTRAINT "Revenue_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Customer_email_key" ON "Customer"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Revenue_month_key" ON "Revenue"("month");
+
+-- AddForeignKey
+ALTER TABLE "Invoice" ADD CONSTRAINT "Invoice_customer_id_fkey" FOREIGN KEY ("customer_id") REFERENCES "Customer"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
---
+++ prisma/seed.ts
@@ -0,0 +1,111 @@
+import { PrismaClient } from '@prisma/client';
+import cuid from 'cuid';
+
+const prisma = new PrismaClient();
+
+// User Data
+const users = [
+  {
+    id: cuid(),
+    name: 'John Doe1',
+    email: 'johndoe1@example.com',
+    password: 'password123',
+  },
+  {
+    id: cuid(),
+    name: 'Jane Doe2',
+    email: 'janedoe2@example.com',
+    password: 'password456',
+  },
+  // Add more user data as needed
+];
+
+// Customer Data
+const customers = [
+  {
+    id: cuid(),
+    name: 'Customer 1',
+    email: 'customer1@example.com',
+    image_url: 'https://example.com/image1.jpg',
+  },
+  {
+    id: cuid(),
+    name: 'Customer 2',
+    email: 'customer2@example.com',
+  },
+  // Add more customer data as needed
+];
+
+// Invoice Data
+const invoices = [
+  {
+    id: cuid(),
+    customer_id: customers[0].id,
+    amount: 100.0,
+    status: 'paid',
+    date: new Date('2023-12-01'),
+  },
+  {
+    id: cuid(),
+    customer_id: customers[0].id,
+    amount: 50.0,
+    status: 'pending',
+    date: new Date('2023-12-10'),
+  },
+  {
+    id: cuid(),
+    customer_id: customers[1].id,
+    amount: 75.0,
+    status: 'paid',
+    date: new Date('2023-12-05'),
+  },
+  // Add more invoice data as needed
+];
+
+// Revenue Data
+const revenues = [
+  {
+    id: cuid(),
+    month: 'January',
+    revenue: 500.0,
+  },
+  {
+    id: cuid(),
+    month: 'February',
+    revenue: 750.0,
+  },
+  {
+    id: cuid(),
+    month: 'March',
+    revenue: 1000.0,
+  },
+  // Add more revenue data as needed
+];
+
+async function main() {
+  try {
+    await prisma.user.createMany({
+      data: users,
+    });
+
+    await prisma.customer.createMany({
+      data: customers,
+    });
+
+    await prisma.invoice.createMany({
+      data: invoices,
+    });
+
+    await prisma.revenue.createMany({
+      data: revenues,
+    });
+
+    console.log('Seed data created successfully!');
+  } catch (error) {
+    console.error('Error seeding the database:', error);
+  } finally {
+    await prisma.$disconnect();
+  }
+}
+
+main();
