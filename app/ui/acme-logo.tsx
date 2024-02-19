220f9c4810eb22181bda7804d5834667c954bb5c
Abdul Alim <aa.abdulalim13@gmail.com>
2024-02-19 08:43:34 +0600

    updated

--- app/ui/acme-logo.tsx
+++ app/ui/acme-logo.tsx
@@ -1,4 +1,7 @@
-import { GlobeAltIcon } from '@heroicons/react/24/outline';
+import {
+  BuildingStorefrontIcon,
+  GlobeAltIcon,
+} from '@heroicons/react/24/outline';
 import { lusitana } from '@/app/ui/fonts';
 
 export default function AcmeLogo() {
@@ -6,8 +9,10 @@ export default function AcmeLogo() {
     <div
       className={`${lusitana.className} flex flex-row items-center leading-none text-white`}
     >
-      <GlobeAltIcon className="h-12 w-12 rotate-[15deg]" />
-      <p className="text-[44px]">Acme</p>
+      {/* <GlobeAltIcon className="h-12 w-12 rotate-[15deg]" /> */}
+      <BuildingStorefrontIcon className="h-12 w-12 rotate-[15deg]" />
+      {/* <p className="text-[44px]">Acme</p> */}
+      <p className="text-[44px]">DAS</p>
     </div>
   );
 }
